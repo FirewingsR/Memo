6f799958f2dc43914873bf003a7eccc6cdcdd084
firewings.r@gmail.com <firewings.r@gmail.com>
2016-11-23 01:16:37 +0800

    算法：选择排序

---
+++ src/com/hyprice/william/algorithms/BaseSort.java
@@ -0,0 +1,18 @@
+package com.hyprice.william.algorithms;
+
+public abstract class BaseSort {
+	public abstract void sort(int[] a);
+	// 交换
+	public void swap (int[] a, int x, int y) {
+		int temp = a[x];
+		a[x] = a[y];
+		a[y] = temp;
+	}
+	// 打印完整序列
+    public void printAll(int[] list) {
+        for (int value : list) {
+            System.out.print(value + "\t");
+        }
+        System.out.println();
+    }
+}
---
+++ src/com/hyprice/william/algorithms/SelectionSort.java
@@ -0,0 +1,45 @@
+package com.hyprice.william.algorithms;
+
+import java.util.Random;
+
+public class SelectionSort extends BaseSort {
+
+	public static void main(String[] args) {
+		final int MAX_SIZE = 10;
+		int[] array = new int[MAX_SIZE];
+		Random random = new Random();
+		for (int i = 0; i < MAX_SIZE; i++) {
+			array[i] = random.nextInt(MAX_SIZE * 10);
+		}
+
+		// 调用选择排序方法
+		SelectionSort selection = new SelectionSort();
+		System.out.print("排序前:\t");
+		selection.printAll(array);
+		selection.sort(array);
+		System.out.print("排序后:\t");
+		selection.printAll(array);
+
+	}
+
+	@Override
+	public void sort(int[] a) {
+		// 需要遍历获得最小值的次数
+		// 要注意一点，当要排序 N 个数，已经经过 N-1 次遍历后，已经是有序数列
+		for (int i = 0; i < a.length - 1; i++) {
+			// 用来保存最小值得索引
+			int index = i;
+			// 寻找第i个小的数值
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[index] > a[j]) {
+					index = j;
+				}
+			}
+			// 将找到的第i个小的数值放在第i个位置上
+			swap(a, index, i);
+			System.out.format("第 %d 趟:\t", i + 1);
+			printAll(a);
+		}
+	}
+
+}
