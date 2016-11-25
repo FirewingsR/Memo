1cdadc1dc9e1b3fb15f13a1d4884d143f0bf4222
William <rongtao@360.cn>
2016-11-25 20:29:16 +0800

    二分查找

---
+++ src/com/hyprice/william/questions/BinarySearch.java
@@ -0,0 +1,21 @@
+package com.hyprice.william.questions;
+
+public class BinarySearch {
+	public int search(Integer[] srcArray, int des) {
+		int low = 0;
+		int high = srcArray.length - 1;
+
+		while ((low <= high) && (low <= srcArray.length - 1) && (high <= srcArray.length - 1)) {
+			int middle = (high + low) >> 1;
+			if (des == srcArray[middle]) {
+				return middle;
+			} else if (des < srcArray[middle]) {
+				high = middle - 1;
+			} else {
+				low = middle + 1;
+			}
+		}
+		
+		return -1;
+	}
+}
