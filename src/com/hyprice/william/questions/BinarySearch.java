80c730707a300177efad126b0f4a1526cc45555b
firewings.r@gmail.com <firewings.r@gmail.com>
2016-11-27 00:23:43 +0800

    复习

--- src/com/hyprice/william/questions/BinarySearch.java
+++ src/com/hyprice/william/questions/BinarySearch.java
@@ -1,21 +1,33 @@
 package com.hyprice.william.questions;
 
+/*
+ * 已理解
+ */
 public class BinarySearch {
-	public int search(Integer[] srcArray, int des) {
+	
+	private static int search(int[] arr, int des) {
 		int low = 0;
-		int high = srcArray.length - 1;
+		int high = arr.length - 1;
 
-		while ((low <= high) && (low <= srcArray.length - 1) && (high <= srcArray.length - 1)) {
+		final int maxIndex = arr.length - 1;
+
+		while ((low <= high) && (low <= maxIndex) && (high <= maxIndex)) {
 			int middle = (high + low) >> 1;
-			if (des == srcArray[middle]) {
+			if (des == arr[middle]) {
 				return middle;
-			} else if (des < srcArray[middle]) {
+			} else if (des < arr[middle]) {
 				high = middle - 1;
 			} else {
 				low = middle + 1;
 			}
 		}
-		
+
 		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = new int[] { 2, 2323, 4, 67, 8, 523, 343, 5, 4324324 };
+		int index = search(arr, 8);
+		System.out.println(index);
 	}
 }
